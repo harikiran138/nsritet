de778d184e2ba21ade981f1ee7b5aef8cf88b353
harikiran138 <155863015+harikiran138@users.noreply.github.com>
2025-11-13 11:23:42 +0530

    chnages section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import HeroSection from '@/components/HeroSection';
 import Highlights from '@/components/Highlights';
 import LatestStories from '@/components/LatestStories';
 import AdmissionsSection from '@/components/AdmissionsSection';
+import OverviewSection from '@/components/OverviewSection';
 import NSRIETLocation from '@/components/NSRIETLocation';
 import AnimatedSection from '@/components/AnimatedSection';
 
@@ -22,14 +23,19 @@ export default function Home() {
         <Highlights />
       </AnimatedSection>
 
-      {/* --- Latest Stories & Events Section --- */}
-      <AnimatedSection className="py-16 sm:py-20 bg-gray-50 dark:bg-gray-900">
-        <LatestStories />
-      </AnimatedSection>
-
       {/* --- Admissions Section --- */}
       <AnimatedSection className="py-16 sm:py-20">
         <AdmissionsSection />
+      </AnimatedSection>
+
+      {/* --- Overview Section --- */}
+      <AnimatedSection className="py-16 sm:py-20 bg-gray-50 dark:bg-gray-800">
+        <OverviewSection />
+      </AnimatedSection>
+
+      {/* --- Latest Stories & Events Section --- */}
+      <AnimatedSection className="py-16 sm:py-20 bg-white dark:bg-gray-900">
+        <LatestStories />
       </AnimatedSection>
 
       {/* --- NSRIET Location Section --- */}
